c13d4bee1a8459c205f4592d67fb7a297d65c7a3
BHunt <benjaminhunt93@gmail.com>
2022-08-10 12:25:43 -0400

    add exact match and contains match to AkuzSearch and EngSearch; add search to notes , examples, and english gloss; implement foma parse before AkuzSearch

--- buttonSearch.js
+++ buttonSearch.js
@@ -1,8 +1,6 @@
-
 //Old buttonSearch
-
 const buttonSearch = (e) => {
-    const searchString = searchInput.value.toLowerCase();
+    var searchString = searchInput.value.toLowerCase();
     const filteredLexicon = LEX.filter((word) =>
 
         word.search_word.toLowerCase().includes(searchString) ||
@@ -12,78 +10,135 @@ const buttonSearch = (e) => {
     displayWords(filteredLexicon);
 };
 
-function akuzSearch(term){
-    const searchString = term;
+function exactMatch(term){
+    let searchString = term;
 
     //Exact match
-    const exactWord = LEX.filter((word) =>
-        word.search_word.toLowerCase() == searchString
-    );
+    let exactWord = LEX.filter((word) =>
+    word.search_word.toLowerCase() == searchString
+);
+    return exactWord;
+}
 
+function containsMatch(term){
+    let searchString = term;
 
     //contains word
-    const containsWord = LEX.filter((word) =>
-        word.search_word.toLowerCase().includes(searchString)
-    );
+    let containsWord = LEX.filter((word) =>
+    word.search_word.toLowerCase().startsWith(searchString)
+);
+    return containsWord;
+}
 
+function printSearch(displayTerm, first, second = []){
     //Print results in section
-    results.innerHTML += `<span class="results_section">Results for <i>${term}</i>:</span>`
-    let termResults = exactWord.concat(containsWord)
+    results.innerHTML += `<span class="results_section">Results for <i>${displayTerm}</i>:</span>`;
+    let termResults = first.concat(second.filter((item) => first.indexOf(item) < 0));
     displayWords(termResults);
+}
 
+function akuzSearch(term){
     //parse the searchTerm
-    
     let parsedRootList = parseWord(term);
 
-    let parsedRoot = parsedRootList[0];
-    let postBases = parsedRootList.slice(1,parsedRootList.length);
+    if(parsedRootList){
+        
+        let initialMatch = exactMatch(term);
+        if(initialMatch.length){
+            printSearch(term, initialMatch);
+        }
 
-    //exact match parsed root
-    const exactParsedRoot = LEX.filter((word) =>
-        word.search_word.toLowerCase() == parsedRoot
-    );
+        let parsedRoot = parsedRootList[0];
+        var postBases = parsedRootList.slice(1,parsedRootList.length);
+        setParse(parsedRootList);
 
-    //contains parsed root
-    const containsParsedRoot = LEX.filter((word) =>
-        word.search_word.toLowerCase().includes(parsedRoot)
-    );
+        //exact match parsed root
+        var exactParsedRoot = exactMatch(parsedRoot);
+        var containsParsedRoot = containsMatch(parsedRoot);
+        printSearch(parsedRoot, exactParsedRoot, containsParsedRoot);
 
-    //Print results in section
-    results.innerHTML += `<span class="results_section">Results for <i>${parsedRoot}</i>:</span>`
-    let parsedResults = exactParsedRoot.concat(containsParsedRoot)
-    displayWords(parsedResults);
-
-    //exact match postbases
-    let postBaseResults = new Array;
-    postBases.foreach((item) =>{
-        var pb = LEX.filter((word) => 
-            word.search_word.toLowerCase() == item
+        //exact match postbases
+        for(let i=0; i < postBases.length-1; i++){
+            var pb = LEX.filter((word) =>
+                word.search_word.toLowerCase() == postBases[i]
             );
-        results.innerHTML += `<span class="results_section">Results for <i>${item}</i>:</span>`
-        displayWords(pb);
-    });
-
+            results.innerHTML += `<span class="results_section">Results for <i>${postBases[i]}</i>:</span>`
+            displayWords(pb);
+        }
+    }
+    //exact and contains searches if word could not be parsed
+    else{
+        var exactWord = exactMatch(term);
+        var containsWord = containsMatch(term);
+        printSearch(term, exactWord, containsWord);
+    }
 };
 
 //Search for English words
 function englishSearch(term) {
-    let filteredLexicon = LEX.filter((word) =>
+    //search for exact match in gloss first
+    let filteredGlossExact = LEX.filter((word) =>
 
-        word.gloss.some(gloss => gloss.includes(searchString))
+        word.gloss.some(gloss => gloss == term)
     );
+
+    //search for contained in gloss
+    let filteredGloss = LEX.filter((word) =>
+
+        word.gloss.some(gloss => gloss.includes(term))
+    );
+
+    //search for match in examples
+    let filteredExamples = LEX.filter((word) =>
+
+        word.examples.some(example => example.includes(term))
+    );
+
+    //search for match in notes
+    let filteredNotes = LEX.filter((word) =>
+
+        word.notes.some(note => note.includes(term))
+    );
+
+    //join all results together
+    let filteredLexicon = filteredGlossExact.concat(filteredGloss.filter((item) => filteredGlossExact.indexOf(item) <0));
+    filteredLexicon = filteredLexicon.concat(filteredExamples.filter((item) => filteredLexicon.indexOf(item) < 0));
+    filteredLexicon = filteredLexicon.concat(filteredNotes.filter((item) => filteredLexicon.indexOf(item) < 0));
 
     displayWords(filteredLexicon);
 };
 
 const searchController = (e) => {
+    //clear out results and parse divs
     results.innerHTML = "";
+    dummy = "";
+    setParse(dummy);
 
     const searchString = searchInput.value.toLowerCase();
 
+    //English search
     if(document.getElementById("engSearch").checked){
         englishSearch(searchString);
     }
+    //Akuzipik search
     else if(document.getElementById("akuzSearch").checked){
         akuzSearch(searchString);
+    }
+};
+
+const setParse = (token) => {
+    if(token == ""){
+        document.getElementById("parse").style.display = "none";
+    }
+    else{
+        document.getElementById("parse").style.display = "flex";
+        const morphs = document.getElementById("morphs");
+        let output = `<span class='base'>${token[0]}</span>`;
+    
+        for(i=1; i<token.length; i++){
+            output += `<span class='morpheme'>+ ${token[i]}</span>`;
+        }
+    
+        morphs.innerHTML = output;
     }
 };
