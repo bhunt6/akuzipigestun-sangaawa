08e1cdcd8ce396e0f84daff816f4f5452f6f81cd
BHunt <benjaminhunt93@gmail.com>
2022-08-10 12:26:18 -0400

    change [] to // for ipa

--- displayWords.js
+++ displayWords.js
@@ -7,7 +7,7 @@ const displayWords = (words) => {
                 <div>
                     <a href="entry.html?entry=${word.UUID}"><span class="entryHead">${word.headword}, ${word.pos}</span></a>
                     <span class="entryCyr">(${word.cyrillic})</span>
-                    <span class="entryipa">[${word.ipa}]</span>
+                    <span class="entryipa">/${word.ipa}/</span>
                     <span class="entryDef">${word.gloss.join("; ")}</span>
                 </div>
                 <span class="entryOther">${word.examples.join("\n")}</span>
