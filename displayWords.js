5faa7cc9531d426e5d8ef58d2528c0ceadb44136
BHunt <benjaminhunt93@gmail.com>
2023-02-13 15:58:26 -0500

    make entire entry clickable

--- displayWords.js
+++ displayWords.js
@@ -3,15 +3,15 @@ const displayWords = (words) => {
     const htmlString = words
         .map((word) =>
         //entry formatting as string
-            `<div class="result">
+            `<a href="entry.html?entry=${word.UUID}" class="result">
                 <div>
-                    <a href="entry.html?entry=${word.UUID}"><span class="entryHead">${word.headword}, ${word.pos}</span></a>
+                    <span class="entryHead">${word.headword}, ${word.pos}</span>
                     <span class="entryCyr">(${word.cyrillic})</span>
                     <span class="entryipa">/${word.ipa}/</span>
                     <span class="entryDef">${word.gloss.join("; ")}</span>
                 </div>
                 <span class="entryOther">${word.examples.join("\n")}</span>
-            </div>
+            </a>
             `)
         .join('');
     results.innerHTML += htmlString;
