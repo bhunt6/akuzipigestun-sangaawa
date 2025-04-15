30108a4d79ea096ac7feb76dc2a67657f973005d
BHunt <benjaminhunt93@gmail.com>
2025-04-15 15:16:23 -0500

    implement per-mood inflection lists

--- dictionary_js/wordBuilder.js
+++ dictionary_js/wordBuilder.js
@@ -4,31 +4,30 @@ function buildController(root, pos) {
         "emotional root":"(EMO)",
         "postural root":"(POS)",
         "pronoun":"(N)"
-}
-    root += posList[pos];
-    createDropdowns(root, pos);
+    }   
+    let base =  root + posList[pos]; 
+    createDropdowns(root, pos, base);
 }
 
 function getMorphs(){
     let morphList = document.getElementsByClassName("include");
     let values = [];
-    let result, gen;
+    let result, gen, finalInfl;
 
-    values[0] = morphList[0].textContent;
+    values[0] = morphList[0].id;
     for(let i=1; i<morphList.length; i++){
         values.push(morphList[i].value);
     }
     let joinedInfl = values.slice(1).join("^");
-    if(joinedInfl in verbExeptions){
-        joinedInfl = verbExeptions[joinedInfl];
+    finalInfl = joinedInfl;
+    
+    if(joinedInfl in verbExceptions){
+        finalInfl = verbExceptions[joinedInfl];
     }
-    gen = values[0] + "^" + joinedInfl;
-    console.log(gen);
-    //result = foma_apply_down(s2m, "qavaghtuq");
+    
+    gen = values[0] + "^" + finalInfl;
  
     result = foma_apply_down(m2s, gen);
-    console.log(result);
-
 
     //print error message
     if (result === undefined || result.length == 0) {
@@ -60,21 +59,16 @@ function fomaPrintInvert(tkn, valid){
 function populate(s1, s2){
     let caseMood = document.getElementById(s1);
     let infl = document.getElementById(s2);
-    let nounCase = caseMood.value;
     let optionArray = [];
     infl.innerHTML = "";
-    
+    let inflSet = caseMood.value;
+
     if(s1 == "case"){
-        optionArray = nounInfl[nounCase];
+        optionArray = nounInfl[inflSet];
     }
 
     else if (s1 == "mood"){
-        if(caseMood.value.includes("Intr")){
-            optionArray = verbIntr;
-        }
-        else{
-            optionArray = verbTrns;
-        }
+        optionArray = verbMoods[inflSet];
     }
 
     for (let option in optionArray){
@@ -87,10 +81,10 @@ function populate(s1, s2){
 
 }
 
-function createDropdowns(base, pos){
+function createDropdowns(root, pos, base){
     let builderArea = document.getElementById("wordBuilder");
     if(pos=="noun"){
-        builderArea.innerHTML = `<span class="morph include" id="base">${base}</span>
+        builderArea.innerHTML = `<span class="morph include" id=${base}>${root}</span>
                                 <select class="morph" id="case" onchange="populate(this.id, 'possNum');" autocomplete="off">
                                     <option value="Default" selected disabled>Case</option>
 						            <option value="Absolutive">Absolutive</option>
@@ -108,7 +102,7 @@ function createDropdowns(base, pos){
                                 <div id="results"></div>`
     }
     else if(pos=="verb"){
-        builderArea.innerHTML = `<span class="morph include" id="base">${base}</span>
+        builderArea.innerHTML = `<span class="morph include" id=${base}>${root}</span>
                                 <select class="morph include" id="mood" onchange="populate(this.id, 'perNum');" autocomplete="off">
 						            <option value="default" selected disabled>Mood</option>
                                 </select>
@@ -121,13 +115,13 @@ function createDropdowns(base, pos){
         let moodList = document.getElementById("mood");
         for (let mood in verbMoods){
             let newOpt = document.createElement("option");
-            newOpt.value = verbMoods[mood];
-            newOpt.innerHTML = verbMoods[mood];
+            newOpt.value = mood;
+            newOpt.innerHTML = mood;
             moodList.options.add(newOpt);
         }
     }
     else if(pos=="pronoun"){
-        builderArea.innerHTML =`<span class="morph" id="base">${base}</span>
+        builderArea.innerHTML =`<span class="morph" id=${base}>${root}</span>
                                 <select class="morph" id="prnInfl" autocomplete="off">
 						            <option value="default" selected>Case/Number</option>
 					            </select>
