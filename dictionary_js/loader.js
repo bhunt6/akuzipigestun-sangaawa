df6db0ef23f9c18db614409d56161c7396fe52f9
BHunt <benjaminhunt93@gmail.com>
2025-05-22 01:41:03 -0500

    loader styling

--- dictionary_js/loader.js
+++ dictionary_js/loader.js
@@ -3,13 +3,13 @@ document.onreadystatechange = function () {
     if (["uninitialized", "loading", "loaded", "interactive"].includes(state)) {
         document.getElementById('entry').style.visibility="hidden";
         document.getElementById('wheel').style.visibility="hidden";
-        document.getElementById('tables').style.visibility="hidden";
+        document.getElementById('wordBuilder').style.visibility="hidden";
     } else if (state == 'complete') {
         setTimeout(function(){
            document.getElementById('lds-grid').style.visibility="hidden";
            document.getElementById('entry').style.visibility="visible";
            document.getElementById('wheel').style.visibility="visible";
-           document.getElementById('tables').style.visibility="visible";
+           document.getElementById('wordBuilder').style.visibility="visible";
 
         },1000);
     }
