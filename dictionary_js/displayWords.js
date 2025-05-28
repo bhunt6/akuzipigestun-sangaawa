8512090a5682bea9cc35825c78f881e1bb1bb4b7
BHunt <benjaminhunt93@gmail.com>
2025-05-28 16:09:18 -0500

    call setTag() when displaying words

--- dictionary_js/displayWords.js
+++ dictionary_js/displayWords.js
@@ -3,25 +3,41 @@ const displayWords = (words) => {
     const htmlString = words
         .map((word) =>
         //entry formatting as string
-            `<a href="entry.html?entry=${word.UUID}" class="result">
-                <div class="entryContent">
-                    <div class="entryTop">
-                        <span class="entryHead">
+            `<div class="entryContent result">
+                <div class="entryTop">
+                    <span class="entryHead">
+                        <a href="entry.html?entry=${word.UUID}" class="headwordlink">
                             <div class="headwordCont">${word.headword}</div>
-                            <div class="tagCont">${word.tags}${word.pos}</div>
-                        </span>
-                        
-                    </div>
-                    <div class="entryBottom">
-                        <span class="entryDef">${word.gloss.join("; ")}</span>
-                    </div>
+                        </a>
+                        <div class="tagCont">${word.tags}${word.pos}</div>
+                    </span>
                 </div>
-            </a>
+                <div class="entryBottom">
+                    <span class="entryDef">${word.gloss.join("; ")}</span>
+                </div>
+            </div>
             `)
         .join('');
     results.innerHTML += htmlString;
+
+    setTag();
 };
 //code for displaying examples on card
 //<span class="entryOther">${word.examples.join("\n")}</span>
 //<span class="entryCyr">(${word.cyrillic})</span>
 //<span class="entryipa">/${word.ipa.join("/, /")}/</span>
+
+
+            //<a href="entry.html?entry=${word.UUID}" class="result">
+            //     <div class="entryContent">
+            //         <div class="entryTop">
+            //             <span class="entryHead">
+            //                 <div class="headwordCont">${word.headword}</div>
+            //                 <div class="tagCont">${word.tags}${word.pos}</div>
+            //             </span>     
+            //         </div>
+            //         <div class="entryBottom">
+            //             <span class="entryDef">${word.gloss.join("; ")}</span>
+            //         </div>
+            //     </div>
+            // </a>
