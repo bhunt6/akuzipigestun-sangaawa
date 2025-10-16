df929460c1b8e90aaa764e135ac7db3edb2bb214
BHunt <benjaminhunt93@gmail.com>
2025-10-16 11:40:01 -0500

    fix tag reference in filter

--- dictionary_js/tagSearch.js
+++ dictionary_js/tagSearch.js
@@ -1,4 +1,4 @@
- //"<span class='tag ChukotkanTag'>CHUKOTKAN</span>"
+ //"<span class='tag chukotkanTag'>CHUKOTKAN</span>"
 function setTag(){
     let tagList = document.getElementsByClassName("tag");
     for(let i=0; i<tagList.length; i++){
@@ -10,16 +10,18 @@ function setTag(){
 function sameTag(tag){
     let results = document.getElementById("results");
     results.innerHTML = "";
-
+    let shortTag = tag.slice(0,-3);
+    console.log(shortTag);
     let tagList = LEX.filter((word) =>
-        word.tags.includes(` ${tag}`) ||
-        word.pos.includes(` ${tag}`)
+        word.tags.includes(shortTag) ||
+        word.pos.includes(shortTag)
     );
     let pbTagList = pbLEX.filter((word) =>
-        word.tags.includes(` ${tag}`) ||
-        word.pos.includes(` ${tag}`)
+        word.tags.includes(shortTag) ||
+        word.pos.includes(shortTag)
     );
 
+    console.log(tagList);
     let allList = tagList.concat(pbTagList.filter((item) => tagList.indexOf(item) <0));
     //let filteredLexicon = filteredGlossExact.concat(filteredGloss.filter((item) => filteredGlossExact.indexOf(item) <0));
 
